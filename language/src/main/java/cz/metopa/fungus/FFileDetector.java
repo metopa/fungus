41668befa179c83d68c0ecb6db430e90be580538
Viacheslav Kroilov <slavakroilov@gmail.com>
2019-12-13 00:25:27 +0100

    Add dummy language launcher

---
+++ language/src/main/java/cz/metopa/fungus/FFileDetector.java
@@ -0,0 +1,22 @@
+package cz.metopa.fungus;
+
+import com.oracle.truffle.api.TruffleFile;
+
+import java.nio.charset.Charset;
+
+public final class FFileDetector implements TruffleFile.FileTypeDetector {
+
+  @Override
+  public String findMimeType(TruffleFile file) {
+    String name = file.getName();
+    if (name != null && name.endsWith(".fun")) {
+      return SLLanguage.MIME_TYPE;
+    }
+    return null;
+  }
+
+  @Override
+  public Charset findEncoding(TruffleFile file) {
+    return null;
+  }
+}
